88ff992551322fe7d6043d414ee1fb61ac80bdc6
DDDlucky <d.ono2105@gmail.com>
2021-05-30 12:38:26 +0900

    0530

--- 見積書作成.js
+++ 見積書作成.js
@@ -15,8 +15,60 @@ function getInfo() {
   var items2 = sheetForm.getRange(lastRow, 9, 1, 4).getValues();
   var items3 = sheetForm.getRange(lastRow, 13, 1, 4).getValues();
 
-  Logger.log(companyName);
-  Logger.log(person);
-  Logger.log(items1);
-  Logger.log(items2);
+  // ６列分に変換
+  items1[0].splice(1, 0, '', '');
+  items2[0].splice(1, 0, '', '');
+  items3[0].splice(1, 0, '', '');
+
+  // 見積書のテンプレ
+  var ssQuotation = SpreadsheetApp.openById('1P_jiTAQYKlmXmQD2Guo4jll6Vfq39wJh91B_MohD2XM')
+  // var sheetQuotation = ssQuotation.getSheetByName('見積書_書き込み用');
+
+  // シートの複製
+  var now = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'yyyyMMdd_hhmm');
+  var sheetName = now + '' + companyName;
+  var sheetQuotation = ssQuotation.duplicateActiveSheet().setName(sheetName);
+  var sheetId = sheetQuotation.getSheetId();
+  sheetQuotation.activate();
+
+  // 書き込み
+  sheetQuotation.getRange('A4').setValue(companyName);
+  sheetQuotation.getRange('A5').setValue(person + ' 様');
+  sheetQuotation.getRange('F12').setValue(documentNumber);
+
+  sheetQuotation.getRange('A17:F17').setValues(items1);
+  sheetQuotation.getRange('A18:F18').setValues(items2);
+  sheetQuotation.getRange('A19:F19').setValues(items3);
+
+  // PDF出力
+  SpreadsheetApp.flush();
+
+  var url = 'https://docs.google.com/spreadsheets/d/1P_jiTAQYKlmXmQD2Guo4jll6Vfq39wJh91B_MohD2XM/export?exportFormat=pdf&gid=SID'.replace('SID', sheetId);
+  var token = ScriptApp.getOAuthToken();
+  var response = UrlFetchApp.fetch(url, {
+    headers: {
+      'Authorizaion': 'Bearer ' + token
+    }
+  });
+
+  var date = Utilities.formatDate(new Date(), 'Asia/Tokyo', 'yyyyMMdd');
+  var blob = response.getBlob().setName(date + '_' + companyName + '御中.pdf');
+
+  var folder = DriveApp.getFolderById('1GcYwpC77f3rZZFI9SlsUjWBuJeN8Fk7i');
+  var file = folder.createFile(blob);
+
+  var to = email;
+  var subject = 'テストメール';
+  var body = 'こちらはテストメールです。';
+  var options = {
+    attachments: [file]
+  };
+
+  GmailApp.sendEmail(
+    to,
+    subject,
+    body,
+    options
+  );
+
 }
